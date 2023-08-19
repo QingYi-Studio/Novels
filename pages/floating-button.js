1bece77e23ab69fb087234abc5b810a36e913eed
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-08-19 22:35:30 +0800

    导入按钮功能

--- pages/floating-button.js
+++ pages/floating-button.js
@@ -1,18 +1,35 @@
-document.getElementById("floating-button").addEventListener("click", function() {
-  // 创建或显示右侧悬浮框
-var floatingBox = document.createElement("div");
-floatingBox.id = "floating-box";
-document.body.appendChild(floatingBox);
-
-// 设置悬浮框样式
-floatingBox.style.position = "fixed";
-floatingBox.style.top = "20px";
-floatingBox.style.right = "20px";
-floatingBox.style.width = "33%";
-floatingBox.style.height = "100%";
-/* 添加其他样式 */
-
-// 获取最近十条GitHub仓库的commits，并将其添加到悬浮框中
-// 使用合适的API和数据处理方法来实现此功能
-
-});
+function toggleFloatBox() {
+      const floatBox = document.getElementById('floatBox');
+      floatBox.classList.toggle('open');
+      
+      if (floatBox.classList.contains('open')) {
+        // 在此处实时获取并展示最近十条commits
+        fetchCommits();
+      } else {
+        // 清空commits内容
+        document.getElementById('commitsContent').innerHTML = '';
+      }
+    }
+    
+    function fetchCommits() {
+      // 使用合适的方法获取commits，并将结果展示在float-box-content中
+      // 以下仅为示例代码，你需要根据实际情况进行修改
+      
+      const url = 'https://api.github.com/repos/Grey-Wind/Novels/commits?per_page=10';
+      
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const commitsContent = document.getElementById('commitsContent');
+          
+          data.forEach((commit) => {
+            const commitElement = document.createElement('a');
+            commitElement.href = commit.html_url; // 将链接指向 GitHub commits 页面
+            commitElement.target = "_blank"; // 在新标签页打开链接
+            commitElement.textContent = commit.commit.message;
+            commitElement.classList.add('float-link'); // 添加样式类
+            commitsContent.appendChild(commitElement);
+          });
+        })
+        .catch((error) => console.log(error));
+    }
